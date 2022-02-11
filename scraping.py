0ba023529871db7bacbde6e69d1be061f9072e09
xJeris <85350673+xJeris@users.noreply.github.com>
2022-02-11 11:45:41 -0500

    updated challenge files

--- scraping.py
+++ scraping.py
@@ -4,21 +4,23 @@ from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
+import time
 
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': 'C:/windows/chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    news_title, news_paragraph = mars_news(browser)
+    #news_title, news_paragraph = mars_news(browser)
     
     # Run all scraping functions and store results in dictionary
     data = {
-      "news_title": news_title,
-      "news_paragraph": news_paragraph,
-      "featured_image": featured_image(browser),
-      "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      #"news_title": news_title,
+      #"news_paragraph": news_paragraph,
+      #"featured_image": featured_image(browser),
+      #"facts": mars_facts(),
+      #"last_modified": dt.datetime.now(),
+      "hemispheres": mars_images(browser) 
     }
 
     # Stop webdriver and return data
@@ -27,7 +29,6 @@ def scrape_all():
 
 
 def mars_news(browser):
-
    # Visit the mars nasa news site
    url = 'https://redplanetscience.com/'
    browser.visit(url)
@@ -99,3 +100,51 @@ if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
+
+def mars_images(browser):
+    # Visit the mars image site
+    base_url = 'https://astrogeology.usgs.gov'
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    time.sleep(1)
+
+    # Convert the browser html to a soup object
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    # Create dictionary to hold image URLs
+    hemispheres = []
+
+    hemi_urls = []
+    for link in img_soup.find_all('a', class_='itemLink'):
+        hemi_urls.append(base_url + link.get('href')) if base_url + link.get('href') not in hemi_urls else hemi_urls
+
+    x = 0
+    
+    # Use for loop to retrieve URLs and append them to the dictionary
+    for hemi_url in hemi_urls:
+    
+        # find link on main page and click it
+        browser.visit(hemi_urls[x])
+        time.sleep(1)
+    
+        # Recall parser for new page
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+
+        # Get image page result
+        hemi_img_url = img_soup.find('a', string='Sample').get('href')
+        hemi_title = img_soup.find('h2', class_='title').get_text()
+
+        # Identify and append URL to the dictionary
+        hemispheres.append({"img_url" : hemi_img_url, "title" : hemi_title})
+    
+        x = x + 1
+    
+        # Return to previous page
+        browser.back()
+
+    return hemispheres
